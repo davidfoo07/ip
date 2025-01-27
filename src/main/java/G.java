2f9c68fa61a92bb199e8c15c8d1767530a7e3b3a
davidfoo007 <tech@myunilah.com>
2025-01-27 16:26:48 +0800

    rename, greet and exit

---
+++ src/main/java/G.java
@@ -0,0 +1,18 @@
+public class G {
+    public static void main(String[] args) {
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        String message = """
+                ____________________________________________________________
+                Hello! I'm [YOUR CHATBOT NAME]
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(message);
+    }
+}
