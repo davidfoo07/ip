ebe85c5ec2c4fc85b5beb77de938209982a18f7b
davidfoo007 <tech@myunilah.com>
2025-01-27 16:54:44 +0800

    echo

--- src/main/java/G.java
+++ src/main/java/G.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class G {
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
@@ -10,9 +12,29 @@ public class G {
                 Hello! I'm G
                 What can I do for you?
                 ____________________________________________________________
+                """;
+
+        System.out.println(message);
+        Scanner scn = new Scanner(System.in);
+        String str1 = scn.nextLine();
+
+        while (!str1.equals("bye")) {
+            String echo = 
+                    "_________________________________\n" +
+                    str1 + "\n" +
+                    "_________________________________\n";
+
+            System.out.println(echo);
+            str1 = scn.nextLine();
+        }
+
+        scn.close();
+
+        String byeMessage = """
+                ____________________________________________________________
                 Bye. Hope to see you again soon!
                 ____________________________________________________________
                 """;
-        System.out.println(message);
+        System.out.print(byeMessage);
     }
 }
