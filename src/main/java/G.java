28a1b279186ffc06b84a56f8e02e54ba070791fe
davidfoo007 <tech@myunilah.com>
2025-01-27 18:03:54 +0800

    Level-3: Mark As Done

--- src/main/java/G.java
+++ src/main/java/G.java
@@ -16,28 +16,67 @@ public class G {
                 """;
 
         System.out.println(message);
-        ArrayList<String> array = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner scn = new Scanner(System.in);
         String str1 = scn.nextLine();
         while (!str1.equals("bye")) {
             if (str1.equals("list")) {
                 System.out.println("_________________________________\n");
+                
+                if (tasks.isEmpty()) {
+                    System.out.println("The list is empty.");
+                }
 
-                for (int i = 0; i < array.size(); i++) {
-                    System.out.println(i + 1 + ". " + array.get(i));
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ". " + "[" + tasks.get(i).getStatusIcon() + "]" + " " + tasks.get(i).getDescription());
                 };
 
                 System.out.println("_________________________________\n");
 
 
             } else {
-                array.add(str1);
-                String echo = 
-                        "_________________________________\n" +
-                        "added: " + str1 + "\n" +
-                        "_________________________________\n";
-    
-                System.out.println(echo);
+                String[] inputArr = str1.split("[\\s]");
+                if (inputArr.length == 2 && inputArr[0].equals("mark")) {
+                    try {
+                        // Attempt to parse the string as an integer
+                        int index = Integer.parseInt(inputArr[1]);
+                        if (index > 0 && index <= tasks.size()) {
+                            Task t = tasks.get(index - 1);
+                            if (t.getStatus()) {
+                                System.out.println("_________________________________\n");
+                                System.out.println("This task has already been marked.");
+                                System.out.println("_________________________________\n");
+                            } else {
+                                t.setStatus();
+                                System.out.println("_________________________________\n" +
+                                "Nice, I've marked this task as done:\n" + "[X]" + " " + tasks.get(index - 1).getDescription());
+                            }
+                        } else {
+                            System.out.println("Index " + index +" out of bounds.");
+                        }
+
+                    } catch (NumberFormatException e) {
+                        // If parsing fails, print "String"
+                        tasks.add(new Task(str1));
+                        String echo = 
+                                "_________________________________\n" +
+                                "added: " + str1 + "\n" +
+                                "_________________________________\n";
+            
+                        System.out.println(echo);
+                    }
+                    
+                } else {
+                    tasks.add(new Task(str1));
+                    String echo = 
+                            "_________________________________\n" +
+                            "added: " + str1 + "\n" +
+                            "_________________________________\n";
+        
+                    System.out.println(echo);
+                }
+
+                
             }
             str1 = scn.nextLine();
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setStatus() {
+        this.isDone = true;
+    }
+
+    public Boolean getStatus() {
+        return this.isDone;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+}
